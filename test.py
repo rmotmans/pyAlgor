5ddaaa864def23ca3c40997556c83cbc06e3d17d
rmotmans <robbemotmans8@gmail.com>
2020-10-13 22:26:39 +0200

    First algorithm

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pyAlgor.test_alg as test
+import pyAlgor.math.math as math
 
 print('test file')
 
@@ -6,3 +7,6 @@ test.test_function('test input')
 
 output = test.squared(4)
 print(output)
+
+print('test gcd:')
+print(math.greatest_common_divisor(49, 21))
