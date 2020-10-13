6ca6509f29f3b0df24ef6536a5bbf51c0e35e84c
rmotmans <robbemotmans8@gmail.com>
2020-10-13 21:42:40 +0200

    Add testing files

---
+++ pyAlgor/test_alg.py
@@ -0,0 +1,7 @@
+def test_function(input):
+    msg = input + ' ' + input
+    print(msg)
+
+
+def squared(number):
+    return number ** 2
---
+++ test.py
@@ -0,0 +1,8 @@
+import pyAlgor.test_alg as test
+
+print('test file')
+
+test.test_function('test input')
+
+output = test.squared(4)
+print(output)
